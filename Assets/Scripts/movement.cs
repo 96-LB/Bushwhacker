aa2d14175300e3d09ffb26d7bf198fcde5776342
Lucas LaBuff <lucasbeef00@gmail.com>
2022-11-13 14:46:53 -0500

    dashy movement

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float rushMaxCooldown = 1;
-    public float rushPower = 4;
+    public float rushPower = 40;
     public float speed = 200;
     public float turnMult = 1;
-    public bool lockedRush = false;
 
     Camera cameraMain;
     Rigidbody2D rb2d;
     float rushTimer;
-    bool rush, rushing;
     Vector3 worldMousePos;
     Quaternion desiredRot;
 
@@ -21,7 +19,7 @@ public class movement : MonoBehaviour
     {
         cameraMain = Camera.main;
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.drag = 1;
+        rb2d.drag = 5;
     }
 
     public void Update()
@@ -30,30 +28,20 @@ public class movement : MonoBehaviour
         Vector2 vRot = new Vector2(worldMousePos.x - transform.position.x, worldMousePos.y - transform.position.y);
         desiredRot = Quaternion.Euler(0, 0, Mathf.Rad2Deg * Mathf.Atan2(vRot.y, vRot.x) - 90);
 
-        if (Input.GetMouseButtonDown(0) && rushTimer > 0)
+        if (Input.GetMouseButtonDown(0) && Time.time > rushTimer)
         {
-            rush = true;
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(transform.up * rushPower);
+            rushTimer = Time.time + rushMaxCooldown;
+            Debug.Log("dash!");
         }
     }
 
 
     public void FixedUpdate()
     {
-        if (Time.time > rushTimer || lockedRush)
-        {
-            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, 0.1f * turnMult);
-        }
-
-        if (rush && Time.time > rushTimer)
-        {
-            rb2d.AddForce(transform.up * rushPower * speed * Time.fixedDeltaTime);
-            rushTimer = Time.time + rushMaxCooldown;
-            rush = false;
-        }
-        else
-        {
-            rb2d.AddForce(transform.up * speed * Time.fixedDeltaTime);
-        }
-
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, 0.1f * turnMult);
+        rb2d.AddForce(transform.up * speed * Time.fixedDeltaTime);
     }
 }
