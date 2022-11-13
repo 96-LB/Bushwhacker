743504e77120d6cf9b3d84f3bf31789e12a51eff
Lucas LaBuff <lucasbeef00@gmail.com>
2022-11-13 14:19:25 -0500

    eddie's movement script

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float rushMaxCooldown;
-    public float rushLength;
-    
-    Camera cameraMain;
+    public float rushMaxCooldown = 1;
+    public float rushPower = 4;
+    public float speed = 200;
+    public float turnMult = 1;
+    public bool lockedRush = false;
 
+    Camera cameraMain;
+    Rigidbody2D rb2d;
     float rushTimer;
     bool rush, rushing;
     Vector3 worldMousePos;
@@ -17,6 +20,8 @@ public class movement : MonoBehaviour
     public void Start()
     {
         cameraMain = Camera.main;
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.drag = 1;
     }
 
     public void Update()
@@ -34,13 +39,21 @@ public class movement : MonoBehaviour
 
     public void FixedUpdate()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, 0.1f);
-        if (rush)
+        if (Time.time > rushTimer || lockedRush)
         {
-            if (!rushing)
-            {
-                
-            }
+            transform.rotation = Quaternion.Slerp(transform.rotation, desiredRot, 0.1f * turnMult);
         }
+
+        if (rush && Time.time > rushTimer)
+        {
+            rb2d.AddForce(transform.up * rushPower * speed * Time.fixedDeltaTime);
+            rushTimer = Time.time + rushMaxCooldown;
+            rush = false;
+        }
+        else
+        {
+            rb2d.AddForce(transform.up * speed * Time.fixedDeltaTime);
+        }
+
     }
 }
