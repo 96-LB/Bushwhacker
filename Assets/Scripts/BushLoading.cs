ee3596f11d43fdf9f38fd2a51b270e57816b218f
Lucas LaBuff <lucasbeef00@gmail.com>
2022-11-13 15:50:40 -0500

    bush loading

---
+++ Assets/Scripts/BushLoading.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushLoading : MonoBehaviour
+{
+
+    public void Start()
+    {
+        debug(LoadImage("a"));
+        debug(LoadImage("b"));
+    }
+
+    static void debug(bool[][] a) {
+        string o = "";
+        foreach (bool[] i in a)
+        {
+            foreach (bool j in i)
+                o += j ? 1 : 0;
+            o += "\n";
+        }
+        Debug.Log(o);
+
+    }
+
+
+    static bool[][] LoadImage(string name)
+    {
+        Texture2D texture = (Texture2D)Resources.Load($"bushes/{name}");
+
+        bool[][] output = new bool[texture.height][];
+        for (int i = 0; i < texture.height; i++)
+        {
+            output[i] = new bool[texture.width];
+            for (int j = 0; j < texture.width; j++)
+            {
+                output[i][j] = texture.GetPixel(j, texture.height - 1 - i) == Color.black;
+            }
+        }
+
+        return output;
+    }
+
+}
